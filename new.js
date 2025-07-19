53f506d0e77a4dd024a33e4e46dd6d3f18f403de
uyoon <fathimmathuuyoon@gmail.com>
2025-07-19 14:01:00 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,20 @@
+// const element = document.getElementById("demo")
+// console.log(element.innerHTML)
+// element.innerHTML = "Hello"
+// const style = element.style
+// style.color = "yellow"
+// style.fontSize = "40px"
+// style.fontFamily = "Arial"
+
+// const element = document.getElementsByClassName("demo1")
+// console.log(element)
+// element[0].innerHTML = "<h1>hello</h1>"
+// element[1].innerHTML = "<h1>hy</h1>"
+// element[2].innerHTML = "<h1>heyy</h1>"
+
+
+// const style = element[2].style
+
+// style.color = "green"
+// style.fontSize = "30px"
+// style.fontFamily = "Arial"
